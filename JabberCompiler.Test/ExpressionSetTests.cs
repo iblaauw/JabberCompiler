1179b5e04735da203f3e07067dbba6d375a9f982
naibla8358 <naibla8358@gmail.com>
2015-08-29 17:55:57 -0400

    Finished with tests.

---
+++ JabberCompiler.Test/ExpressionSetTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JabberCompiler.Model.Mutable;
+
+namespace JabberCompiler.Test
+{
+    [TestClass]
+    public class ExpressionSetTests
+    {
+        private IExpressionSet set;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            set = CreationUtilities.CreateExpressionSet();
+        }
+
+        [TestMethod]
+        public void ExpressionSetBasics()
+        {
+            Assert.IsNotNull(set);
+            Assert.ReferenceEquals(set.AssociateContext, set.AssociatedContextMutable);
+            Assert.IsTrue(set.Expressions.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void ExpressionSetAdd()
+        {
+            IExpression expression = set.AddExpression();
+
+            Assert.IsNotNull(expression);
+            Assert.ReferenceEquals(expression.OwningContext, set.AssociateContext);
+        }
+    }
+}
